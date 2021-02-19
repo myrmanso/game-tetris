b1dd9f382862c1601941c570664ac7b063ba6559
mmanso <mayara.manso@mercadolivre.com>
2021-02-18 22:04:44 -0300

    creates board

--- app/js/index.js
+++ app/js/index.js
@@ -9,7 +9,16 @@ context.scale(20, 20)
 context.fillStyle = '#899874';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
+//creates board
+let board = [];
+for (let l = 0; l < 20; l++) {
+  board[l] = [];
+  for (let c = 0; c < 10; c++) {
+    board[l][c] = 0;
+  }
+}
 
+console.group(board)
 
 
 

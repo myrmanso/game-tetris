8243c878ee25530ead992a0b3fc1b32ea6983420
mmanso <mayara.manso@mercadolivre.com>
2021-02-26 15:28:50 -0300

    creates drawPlayer

--- app/js/Player/Player.js
+++ app/js/Player/Player.js
@@ -1,15 +1,25 @@
 import ELement from '../Element/Element';
 
 class Player extends ELement {
-  coonstructor(context, canvas, positionX, positionY, width, heigth, color, matrix) {
-    super(context, canvas, positionX, positionY, width, heigth)
+  coonstructor(context, canvas, posX, posY, width, heigth, piece) {
+    super(context, canvas, posX, posY, width, heigth)
     this.score = 0;
-    this.color = color;
-    this.matrix = matrix;
+    this.color = piece.color;
+    this.matrix = piece.matrix;
   }
 
   drawPlayer() {
-
+    this.matrix.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value !== 0) {
+          this.context.fillStyle = this.color;
+          this.context.fillRect(
+            x + this.posX,
+            y + offset.y,
+            1, 1);
+        }
+      });
+    });
   }
 
 

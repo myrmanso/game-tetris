aa0a6c6c1dfa0e34619094431a8843dafcc73f7f
mmanso <mayara.manso@mercadolivre.com>
2021-02-26 10:34:05 -0300

    Creates class Element

---
+++ app/js/Element/Element.js
@@ -0,0 +1,12 @@
+class Element {
+  coonstructor(context, canvas, posX, posY, width, heigth) {
+    this.context = context;
+    this.canvas = canvas;
+    this.posX = posX;
+    this.posY = posY;
+    this.width = width;
+    this.height = heigth;
+  }
+}
+
+export default Element;

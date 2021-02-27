4131c4754e48df9af5faf4dc09a718c4ac012898
mmanso <mayara.manso@mercadolivre.com>
2021-02-27 01:09:25 -0300

    deletes position and drawMatrix in class Object

--- app/script/Components/Object/Object.js
+++ app/script/Components/Object/Object.js
@@ -1,5 +1,5 @@
 class Object {
-  coonstructor(context, canvas, posX, posY, width, heigth, counter, matrix, color) {
+  constructor(context, canvas, posX, posY, width, heigth, counter, matrix, color) {
     this.context = context;
     this.canvas = canvas;
     this.posX = posX;
@@ -9,27 +9,6 @@ class Object {
     this.counter = counter;
     this.matrix = matrix;
     this.color = color;
-  }
-
-  position() {
-    return {
-      x: this.posX,
-      y: this.posY,
-    }
-  };
-
-  drawMatrix(element) {
-    element.matrix.forEach((row, y) => {
-      row.forEach((value, x) => {
-        if (value !== 0) {
-          this.context.fillStyle = this.color;
-          this.context.fillRect(
-            x + element.position().x,
-            y + element.position().y,
-            1, 1);
-        }
-      });
-    });
   }
 };
 

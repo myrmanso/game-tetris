09056a9d9f9174eb948680cef73eb662fec9e041
mmanso <mayara.manso@mercadolivre.com>
2021-02-27 01:07:09 -0300

    deletes scale and refactor bildBoardMatrix

--- app/script/Components/Board/Board.js
+++ app/script/Components/Board/Board.js
@@ -7,28 +7,16 @@ class Board extends Object {
       numberRow: numberRow,
       numberColumn: numberColumn
     };
-    this.scale = {
-      x: scaleX,
-      y: scaleY,
-    };
-    this.posX = 0;
-    this.posY = 0;
-  }
-
-  scaleBoard() {
-    this.context.scale(this.scale.x, this.scale.y)
   }
 
   fillBoard() {
     this.context.fillStyle = this.color;
-    context.fillRect(this.posX, this.posY, this.width, this.height);
+    this.context.fillRect(this.posX, this.posY, this.width, this.height);
   }
 
   bildBoardMatrix() {
-    const matrixBoard = [];
     while (this.matrixBoardParams.numberColumn--) {
-      matrix.push(new Array(this.matrixBoardParams.numberRow).fill(0));
+      this.matrix.push(new Array(this.matrixBoardParams.numberRow).fill(0));
     }
-    this.matrixBoard = matrixBoard;
   }
 };

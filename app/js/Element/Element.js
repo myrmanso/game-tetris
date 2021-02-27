4563bdd11ba0d881eb48af93bc1be840588e677b
mmanso <mayara.manso@mercadolivre.com>
2021-02-26 21:43:46 -0300

    insert counter in params

--- app/js/Element/Element.js
+++ app/js/Element/Element.js
@@ -1,11 +1,12 @@
 class Element {
-  coonstructor(context, canvas, posX, posY, width, heigth) {
+  coonstructor(context, canvas, posX, posY, width, heigth, counter) {
     this.context = context;
     this.canvas = canvas;
     this.posX = posX;
     this.posY = posY;
     this.width = width;
     this.height = heigth;
+    this.counter = counter;
   }
 }
 

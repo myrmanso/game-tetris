f4e30f00d990de245b057b21c34e2c5c93e3bfc3
mmanso <mayara.manso@mercadolivre.com>
2021-02-27 11:29:16 -0300

    creates colorBoard

--- app/script/index.js
+++ app/script/index.js
@@ -3,8 +3,8 @@ window.onload = () => {
   const context = canvas.getContext('2d');
 
   context.scale(20, 20);
-
-  const board = new Board(context, canvas, 0, 0, canvas.width, canvas.height, '#899874', 20, 12);
+  const colorBoard = '#899874'
+  const board = new Board(context, canvas, 0, 0, canvas.width, canvas.height, colorBoard, 20, 12);
   board.bildBoardMatrix();
 
   const player = new Player(context, canvas, 1, 1, board.matrix);

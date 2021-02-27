e180edac06f1b25e305dd64fe50f8dd9541a1bbc
mmanso <mayara.manso@mercadolivre.com>
2021-02-27 01:08:21 -0300

    insert drawMatrix, fillBoard and doesn't extend object

--- app/script/Components/Game/Game.js
+++ app/script/Components/Game/Game.js
@@ -1,12 +1,26 @@
-class Game extends Object {
+class Game {
   constructor(context, canvas, board, player) {
-    super(context, canvas);
+    this.context = context;
+    this.canvas = canvas;
     this.board = board;
     this.player = player;
   }
 
+  drawMatrix(matrix) {
+    matrix.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value !== 0) {
+          this.context.fillStyle = 'red';
+          this.context.fillRect(x, y, 1, 1);
+        }
+      });
+    });
+  }
+
   draw() {
-    this.drawMatrix(this.board);
-    this.drawMatrix(this.player);
+    this.board.fillBoard();
+
+    this.drawMatrix(this.board.matrix);
+    this.drawMatrix(this.player.matrix);
   }
 };

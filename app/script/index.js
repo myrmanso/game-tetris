4ebb2e19aa7d2a50dc952519dc072ad711bffe9b
mmanso <mayara.manso@mercadolivre.com>
2021-02-27 07:30:15 -0300

    fix board matrxParams, don't pass pieces in player, call createPiece

--- app/script/index.js
+++ app/script/index.js
@@ -4,13 +4,14 @@ window.onload = () => {
 
   context.scale(20, 20);
 
-  const board = new Board(context, canvas, 0, 0, canvas.width, canvas.height, '#899874', 10, 20, 20, 20);
+  const board = new Board(context, canvas, 0, 0, canvas.width, canvas.height, '#899874', 20, 12);
   board.bildBoardMatrix();
 
-  const player = new Player(context, canvas, 1, 1, pieces[0]);
+  const player = new Player(context, canvas, 1, 1, board.matrix);
 
   const gameTetris = new Game(context, canvas, board, player);
 
+  gameTetris.createPiece()
   gameTetris.draw();
   gameTetris.fallingPiece();
   gameTetris.configureKeyboardControls();

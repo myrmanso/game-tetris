e98cbc27b4953d641200090390d0e33af0efdb37
mmanso <mayara.manso@mercadolivre.com>
2021-02-26 21:18:16 -0300

    creates player moves

--- app/js/Player/Player.js
+++ app/js/Player/Player.js
@@ -3,7 +3,6 @@ import ELement from '../Element/Element';
 class Player extends ELement {
   coonstructor(context, canvas, posX, posY, width, heigth, piece) {
     super(context, canvas, posX, posY, width, heigth)
-    this.score = 0;
     this.color = piece.color;
     this.matrix = piece.matrix;
   }
@@ -21,4 +20,41 @@ class Player extends ELement {
       });
     });
   }
+
+  moveLeft() {
+    return this.posX -= 1;
+  }
+
+  moveRight() {
+    return this.posX += 1;
+  }
+
+  movesBottom() {
+    this.counter = 0; s
+    return this.posY++;
+    ;
+  }
+
+  moveRotation() {
+
+  }
+
+
+  moves(keyCode) {
+    switch (keyCode) {
+      case ArrowLeft:
+        this.moveLeft;
+        break;
+      case ArrowRight:
+        this.moveRight;
+        break;
+      case ArrowDown:
+        this.movesBottom;
+        break;
+      default:
+        window.alert('Digite um movimento valido')
+        break;
+    }
+  }
+
 }
